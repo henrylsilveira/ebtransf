b5493c1130a321bd2fbf4a9b647b3899549dcffb
Henry Leão <henrylsilveira@gmail.com>
2023-09-30 16:47:09 -0400

    Alguns ajustes na interface; Ajustes do sitemap

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ import { RiTeamLine } from "react-icons/ri";
 import Suporte from "./SuportePoppover";
 export function SideBar() {
     return (
-        <nav className="fixed top-[40%] border border-l-0 border-green-500 bg-black py-4 bg-opacity-50 rounded-tr-lg rounded-br-lg">
+        <nav className="fixed top-[25%] border border-l-0 border-green-500 bg-black py-4 bg-opacity-50 rounded-tr-lg rounded-br-lg">
             <ul>
                 <li><Link className="text-sm justify-center items-center border-0 p-2 flex " href="/calculadora"><BsCalculator className="pr-1 h-7 w-7 text-2xl hover:text-green-600 transform transition-colors text-white" /></Link></li>
                 <li><Link className="text-sm justify-center items-center border-0 p-2 flex " href="/tempoServico"><BsClock className="pr-1 h-7 w-7 text-2xl text-white hover:text-green-600 transform transition-colors" /></Link></li>
