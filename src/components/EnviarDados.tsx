d680c86a1161087039a067840860f6d68774f6dc
Henry Leao <henrylsilveira@gmail.com>
2024-02-05 16:49:53 -0300

    Correção no tamanho do modal enviar dados

--- src/components/EnviarDados.tsx
+++ src/components/EnviarDados.tsx
@@ -34,7 +34,7 @@ export function EnviarDados({ enviarFunc, data, tipo }: {
             </AlertDialog.Trigger>
             <AlertDialog.Portal>
                 <AlertDialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
-                <AlertDialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[180vw] max-w-[800px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-backgroundColor border border-green-700 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+                <AlertDialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[80vw] max-w-[600px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-backgroundColor border border-green-700 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
                     <AlertDialog.Title className="text-green-700 m-0 text-2xl font-medium">
                         Enviar registros para o Banco de Dados
                     </AlertDialog.Title>
