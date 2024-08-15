79b3b8d19115666525d872e330c8b85783836f76
Henry Leao <henrylsilveira@gmail.com>
2024-08-15 18:47:37 -0300

    finalizada pagina principal com a paginacao de noticias

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -38,7 +38,7 @@ export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
     fetchOptions:
       process.env.NODE_ENV === "production"
         ? { next: { tags: ["prismic"] }, cache: "force-cache" }
-        : { next: { revalidate: 5 } },
+        : { next: { revalidate: 60*60*24 } },
     accessToken: "MC5acmJOQ1JBQUFDb0FTSV9t.V0wdQ--_ve-_ve-_vUhH77-9HCfvv70C77-9bO-_vXd0ce-_ve-_ve-_ve-_ve-_ve-_ve-_ve-_vSzvv71C77-9",
     ...config,
   });
